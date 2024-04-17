86343fec66d5fcba0a84656b0816fe2a08a93ee2
Christopher Altona <caltona1@gmail.com>
2024-04-17 09:50:32 -0400

    Add the ability to filter based on text contents of an entry

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # misc-server
 
-Server just runs a bunch of stuff I find useful
+Server just runs a stuff I find useful
 
 ```
 docker run -p 8080:8080 d3v01d/misc-server:stable
+```
+
+APIS:
+
+```
+GET /rss
+
+PARAMS (supports multiple in request as x=a&x=b
+
+filters=value
+- shorts (remove youtube shorts)
+
+excludetext=value
+- removes entries without the given text
+
+includetext=value
+- removes entries with the given text
+
+EXAMPLES
+- /rss?filter=SHORTS&includetext=Race%20Highlights&url=https://www.youtube.com/feeds/videos.xml?channel_id=UCfDfvvMARk4TKcC62ALi6eA
+-- Removes all shorts and non race highlights from Eurosport Cycling
+```
--- src/main/java/net/caltona/miscserver/RSSController.java
+++ src/main/java/net/caltona/miscserver/RSSController.java
@@ -16,7 +16,6 @@ import org.springframework.web.client.RestTemplate;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -26,13 +25,14 @@ public class RSSController {
     @Autowired
     private RestTemplate restTemplate;
 
-    public enum Filter {
-        SHORTS
-    }
-
     @GetMapping(value = "/rss", produces = "text/xml; charset=UTF-8")
-    public String rss(@RequestParam String url, @RequestParam String filter) throws DocumentException, MalformedURLException {
-        Set<Filter> filters = parseFilters(filter);
+    public String rss(
+            @RequestParam String url,
+            @RequestParam(required = false) List<String> filter,
+            @RequestParam(required = false) List<String> excludetext,
+            @RequestParam(required = false) List<String> includetext
+    ) throws DocumentException, MalformedURLException {
+        List<Filter> filters = parseFilters(url, filter, excludetext, includetext);
         SAXReader saxReader = new SAXReader();
         Document root = saxReader.read(new URL(url));
         Element rootElement = root.getRootElement();
@@ -42,51 +42,144 @@ public class RSSController {
         return root.asXML();
     }
 
-    private Set<Filter> parseFilters(String filter) {
-        if (filter == null) {
-            return Set.of();
-        } else {
-            return Arrays.stream(filter.split(","))
-                    .map(Filter::valueOf)
-                    .collect(Collectors.toSet());
-        }
-    }
-
-    private List<Element> findElementsToDelete(String url, Set<Filter> filters, Element rootElement) {
-        List<Element> elementsToDelete = new ArrayList<>();
-        for (Element childElement : rootElement.elements()) {
-            if (filters.contains(Filter.SHORTS) && "entry".equals(childElement.getName())) {
-                Element id = childElement.element("id");
-                if (id != null) {
-                    String videoId = id.getText();
-                    if (videoId != null) {
-                        String[] split = videoId.split(":");
-                        if (split.length == 3) {
-                            String actualId = split[2];
-                            log.debug("RSS URL [{}] found actual video id [{}]", url, actualId);
-                            if (isShort(actualId)) {
-                                elementsToDelete.add(childElement);
-                                log.info("RSS URL [{}] found short [{}]", url, actualId);
-                            } else {
-                                log.info("RSS URL [{}] found video [{}]", url, actualId);
-                            }
-                        } else {
-                            log.warn("RSS URL [{}] entry has a malformed id in [{}]", url, id.asXML());
-                        }
-                    } else {
-                        log.warn("RSS URL [{}] entry has no video id in [{}]", url, id.asXML());
-                    }
-                } else {
-                    log.warn("RSS URL [{}] entry has no id in [{}]", url, childElement.asXML());
+    private List<Filter> parseFilters(String url, List<String> filter, List<String> excludeText, List<String> includeText) {
+        List<Filter> filters = new ArrayList<>();
+        if (filter != null) {
+            for (String value : filter) {
+                if (value.equalsIgnoreCase("shorts")) {
+                    ShortsFilter shortsFilter = new ShortsFilter();
+                    log.info("RSS URL [{}] added filter [{}]", url, shortsFilter);
+                    filters.add(shortsFilter);
                 }
             }
         }
+        if (excludeText != null) {
+            for (String value : excludeText) {
+                ExcludeTextFilter excludeTextFilter = new ExcludeTextFilter(value.toLowerCase());
+                log.info("RSS URL [{}] added filter [{}]", url, excludeTextFilter);
+                filters.add(excludeTextFilter);
+            }
+        }
+        if (includeText != null) {
+            for (String value : includeText) {
+                IncludeTextFilter includeTextFilter = new IncludeTextFilter(value.toLowerCase());
+                log.info("RSS URL [{}] added filter [{}]", url, includeTextFilter);
+                filters.add(includeTextFilter);
+            }
+        }
+        return filters;
+    }
+
+    public interface Filter {
+
+        boolean shouldFilter(String url, String description, Element element);
+
+    }
+
+    private Set<Element> findElementsToDelete(String url, List<Filter> filters, Element rootElement) {
+        Set<Element> elementsToDelete = new HashSet<>();
+        Set<String> remaining = new LinkedHashSet<>();
+        Set<String> removed = new LinkedHashSet<>();
+        for (Element element : rootElement.elements()) {
+            if (element.getName().equals("entry")) {
+                Element title = element.element("title");
+                String description = title != null ? title.getText() : "No Title";
+                boolean filtered = false;
+                for (Filter filter : filters) {
+                    if (filter.shouldFilter(url, description, element)) {
+                        log.info("RSS URL [{}] [{}] filtered because of [{}]", url, description, filter);
+                        filtered = true;
+                    } else {
+                        log.info("RSS URL [{}] [{}] not filtered because of [{}]", url, description, filter);
+                    }
+                }
+                if (filtered) {
+                    elementsToDelete.add(element);
+                    removed.add(description);
+                } else {
+                    remaining.add(description);
+                }
+            }
+        }
+        log.info("RSS URL [{}] removed [{}] elements left [{}] remaining with titles removed [{}] and remaining [{}]", url, removed.size(), remaining.size(), removed, remaining);
         return elementsToDelete;
     }
 
-    private boolean isShort(String videoId) {
-        HttpHeaders headers = restTemplate.headForHeaders("https://www.youtube.com/shorts/" + videoId);
-        return !headers.containsKey("location");
+    public class ShortsFilter implements Filter {
+
+        @Override
+        public boolean shouldFilter(String url, String description, Element element) {
+            Element id = element.element("id");
+            if (id != null) {
+                String videoId = id.getText();
+                if (videoId != null) {
+                    String[] split = videoId.split(":");
+                    if (split.length == 3) {
+                        String actualId = split[2];
+                        log.debug("RSS URL [{}] [{}] found actual video id [{}]", url, description, actualId);
+                        if (isShort(actualId)) {
+                            log.debug("RSS URL [{}] [{}] found short [{}]", url, description, actualId);
+                            return true;
+                        } else {
+                            log.debug("RSS URL [{}] [{}] found video [{}]", url, description, actualId);
+                        }
+                    } else {
+                        log.warn("RSS URL [{}] [{}] entry has a malformed id in [{}]", url, description, id.asXML());
+                    }
+                } else {
+                    log.warn("RSS URL [{}] [{}] entry has no video id in [{}]", url, description, id.asXML());
+                }
+            } else {
+                log.debug("RSS URL [{}] [{}] entry has no id in [{}]", url, description, element.asXML());
+            }
+            return false;
+        }
+
+        private boolean isShort(String videoId) {
+            HttpHeaders headers = restTemplate.headForHeaders("https://www.youtube.com/shorts/" + videoId);
+            return !headers.containsKey("location");
+        }
+
+        @Override
+        public String toString() {
+            return "ShortsFilter{}";
+        }
+    }
+
+    @AllArgsConstructor
+    public class ExcludeTextFilter implements Filter {
+
+        private String text;
+
+        @Override
+        public boolean shouldFilter(String url, String description, Element element) {
+            return element.asXML().toLowerCase().contains(text);
+        }
+
+        @Override
+        public String toString() {
+            return "ExcludeTextFilter{" +
+                    "text='" + text + '\'' +
+                    '}';
+        }
+    }
+
+    @AllArgsConstructor
+    public class IncludeTextFilter implements Filter {
+
+        private String text;
+
+        @Override
+        public boolean shouldFilter(String url, String description, Element element) {
+            return !element.asXML().toLowerCase().contains(text);
+        }
+
+        @Override
+        public String toString() {
+            return "IncludeTextFilter{" +
+                    "text='" + text + '\'' +
+                    '}';
+        }
     }
 
 }
